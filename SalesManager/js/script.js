b3eeaee48accc672cc3d935de426215aff046778
Salifu Mutaru <salifumutaru@gmail.com>
2015-10-24 09:22:38 +0000

    few changes

--- SalesManager/js/script.js
+++ SalesManager/js/script.js
@@ -13,27 +13,27 @@ function sendRequest(u) {
 $(function () {
   $('#login').submit(function(e) {
     e.preventDefault();
-    testingUserLogin();
+    Login();
   });
 });
 
 
-function testingUserLogin(){
+function Login(){
     /*username*/
-    var testingUserUsername = $("#testingUserUsername").val();
+    var testingUserUsername = $("#username").val();
     /*password*/
-    var testingUserPassword = $("#testingUserPassword").val();
+    var testingUserassword = $("#password").val();
 
     /* empty username */
-    if(testingUserUsername.length == 0){
+    if(username.length == 0){
         document.getElementById("error_area").innerHTML = '<div class="chip red white-text">Empty username<i class="material-icons">close</i></div>';
         return
         }
-    if(testingUserPassword.length == 0){
+    if(password.length == 0){
         document.getElementById("error_area").innerHTML = '<div class="chip red white-text">Empty password<i class="material-icons">close</i></div>';
         return;
     }
-    var strUrl = "logic/controller.php?testingUserUsername="+testingUserUsername+"&testingUserPassword="+testingUserPassword;
+    var strUrl = "logic/controller.php?testingUserUsername="+username+"&testingUserPassword="+password;
     var objResult = sendRequest(strUrl);
     var errorArea = document.getElementById("error_area");
     document.getElementById("error_area").innerHTML = '<div class="progress"><div class="indeterminate"></div></div>';
@@ -41,6 +41,6 @@ function testingUserLogin(){
     document.getElementById("error_area").innerHTML = '<div class="chip red white-text">'+objResult.message+'<i class="material-icons">close</i></div>';
     return;
     }
-    document.getElementById("login_area").innerHTML = '<div class="center"><img src="img/smile.gif" class="login_image" align="middle"/></div><h2 class="center orange-text">Login was Successful!</h2>';
+    window.location.href="home.html";
    /*document.getElementById("login_area").innerHTML = '<div class="center"><video width="320" height="240" controls><source src="img/welcome.mp4" type="video/mp4"><source src="movie.ogg" type="video/ogg">Your browser does not support the video tag.</video>Successful login!</div>';*/
 }
